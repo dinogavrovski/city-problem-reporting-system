cde93ce64fa3b065a97ca6bebdbda608be419911
dinodinac <dino.gavrovski@students.finki.ukim.mk>
2026-03-31 17:58:50 +0200

    Initialize Spring Boot

---
+++ src/main/java/com/example/city_problem_reporting/CityProblemReportingApplication.java
@@ -0,0 +1,13 @@
+package com.example.city_problem_reporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityProblemReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityProblemReportingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/city_problem_reporting/CityProblemReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.city_problem_reporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityProblemReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
